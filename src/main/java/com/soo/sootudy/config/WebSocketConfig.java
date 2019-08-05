c8c8ce007682a21760187f4e3651ed7ea31f2a4b
SooJae <Mynameisleesujae@gmail.com>
2019-08-05 09:42:59 +0900

    test WebSocket

--- src/main/java/com/soo/sootudy/config/WebSocketConfig.java
+++ src/main/java/com/soo/sootudy/config/WebSocketConfig.java
@@ -1,17 +1,12 @@
 package com.soo.sootudy.config;
 
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.soo.sootudy.websocket.ChatHandler;
@@ -36,15 +31,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
 		return new ChatHandler();
 	}
 	
-	@Bean
-	public HandshakeInterceptor interceptors() {
-		return new HandshakeInterceptor();
-	}
 	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		// TODO Auto-generated method stub
-		registry.addHandler(chatHandler(), "/ws").setAllowedOrigins("*").withSockJS().setInterceptors(interceptors);
+		registry.addHandler(chatHandler(), "/echo").setAllowedOrigins("*").withSockJS();
 //		registry.addHandler(chatHandler(), "/ws").setAllowedOrigins("*").withSockJS();
 	}
 }
