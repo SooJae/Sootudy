a3258beec348dafb74669efa4ae26c576e65b0ca
SooJae <Mynameisleesujae@gmail.com>
2019-08-15 09:54:20 +0900

    test properties

--- src/main/java/com/soo/sootudy/config/RootConfig.java
+++ src/main/java/com/soo/sootudy/config/RootConfig.java
@@ -5,18 +5,20 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.soo.sootudy.task.GlobalPropertySource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @ComponentScan(basePackages= {"com.soo.sootudy.service", "com.soo.sootudy.controller","com.soo.sootudy.aop","com.soo.sootudy.task"})
@@ -31,18 +33,28 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableTransactionManagement
 
 @MapperScan(basePackages = {"com.soo.sootudy.mapper"})
+@Slf4j
 public class RootConfig {
 
+	@Autowired
+	public GlobalPropertySource globalPropertySource;
+	
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
 		
+		log.info("global"+globalPropertySource);
 		
-		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mariadb://sootudydb.c6v2froaotfn.ap-northeast-2.rds.amazonaws.com:3306/SOOTUDY");
+//		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+//		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mariadb://sootudydb.c6v2froaotfn.ap-northeast-2.rds.amazonaws.com:3306/SOOTUDY");
+//		
+//		hikariConfig.setUsername("SOOJAE");
+//		hikariConfig.setPassword("tnwo3972!!");
+		hikariConfig.setDriverClassName(globalPropertySource.getDriverClassName());
+		hikariConfig.setJdbcUrl(globalPropertySource.getUrl());
 		 
-		hikariConfig.setUsername("SOOJAE");
-		hikariConfig.setPassword("tnwo3972!!");
+		hikariConfig.setUsername(globalPropertySource.getUsername());
+		hikariConfig.setPassword(globalPropertySource.getPassword());
 		
 		return new HikariDataSource(hikariConfig);
 	}
---
+++ src/main/java/com/soo/sootudy/task/GlobalPropertySource.java
@@ -0,0 +1,32 @@
+package com.soo.sootudy.task;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import lombok.Getter;
+
+@Configuration
+@PropertySources({
+    @PropertySource( value = "file:c:/dev/config.properties", ignoreResourceNotFound = true ),
+    @PropertySource( value = "classpath:/resources/application.properties", ignoreResourceNotFound = true)
+})
+@Getter
+public class GlobalPropertySource {
+ 
+	
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+ 
+}
+
